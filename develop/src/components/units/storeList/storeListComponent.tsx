f10931b3c905b9dd5d85e43001c531cbfc606a5f
Calmer_laptop <ty.calmer96@gmail.com>
2025-01-10 19:14:25 +0900

    chore: vscode 설정 추가

--- develop/src/components/units/storeList/storeListComponent.tsx
+++ develop/src/components/units/storeList/storeListComponent.tsx
@@ -1,77 +1,86 @@
-import * as A from './storeList.styles';
-import SearchComponent from '../../common/search/searchComponent';
-import React, { useEffect } from 'react';
+import * as A from "./storeList.styles";
+import SearchComponent from "../../common/search/searchComponent";
+import React, { useEffect } from "react";
 
 declare const window: typeof globalThis & {
-    kakao: any;
+  kakao: any;
 };
 
 export default function StoreListComponent(): JSX.Element {
-    useEffect(() => {
-        // Kakao 지도 API 스크립트 로드
-        const KAKAO_API_KEY = process.env.NEXT_PUBLIC_KAKAO_API_KEY;
-        const script = document.createElement('script');
-        script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_API_KEY}&autoload=false&libraries=services`;
-        document.head.appendChild(script);
+  useEffect(() => {
+    // Kakao 지도 API 스크립트 로드
+    const KAKAO_API_KEY = process.env.NEXT_PUBLIC_KAKAO_API_KEY;
+    const script = document.createElement("script");
+    script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${KAKAO_API_KEY}&autoload=false&libraries=services`;
+    document.head.appendChild(script);
 
-        // 로컬 스토리지에서 데이터를 가져옵니다.
-        const cachedData = localStorage.getItem('BusinessInfoData');
+    console.log("KAKAO_API_KEY", KAKAO_API_KEY);
 
-        if (cachedData) {
-            const parsedData = JSON.parse(cachedData);
+    // 로컬 스토리지에서 데이터를 가져옵니다.
+    const cachedData = localStorage.getItem("BusinessInfoData");
 
-            // 데이터를 기반으로 사용자 주소를 추출
-            const userAddress = parsedData?.RegionMnyFacltStus?.[1]?.row?.[0]?.REFINE_LOTNO_ADDR;
+    if (cachedData) {
+      const parsedData = JSON.parse(cachedData);
 
-            console.log(userAddress);
+      // 데이터를 기반으로 사용자 주소를 추출
+      const userAddress =
+        parsedData?.RegionMnyFacltStus?.[1]?.row?.[0]?.REFINE_LOTNO_ADDR;
 
-            script.onload = () => {
-                window.kakao.maps.load(() => {
-                    const container = document.getElementById('map'); // 지도를 담을 영역의 DOM 레퍼런스
-                    const map = new window.kakao.maps.Map(container, {
-                        center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 기본 위치 (수정 필요)
-                        level: 3, // 지도의 확대 레벨
-                    });
+      console.log(userAddress);
 
-                    const geocoder = new window.kakao.maps.services.Geocoder();
+      script.onload = () => {
+        window.kakao.maps.load(() => {
+          const container = document.getElementById("map"); // 지도를 담을 영역의 DOM 레퍼런스
+          const map = new window.kakao.maps.Map(container, {
+            center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 기본 위치 (수정 필요)
+            level: 3, // 지도의 확대 레벨
+          });
 
-                    geocoder.addressSearch(userAddress, function (result: any, status: any) {
-                        if (status === window.kakao.maps.services.Status.OK) {
-                            const coords = new window.kakao.maps.LatLng(result[0].y, result[0].x);
+          const geocoder = new window.kakao.maps.services.Geocoder();
 
-                            console.log('coords', coords);
+          geocoder.addressSearch(
+            userAddress,
+            function (result: any, status: any) {
+              if (status === window.kakao.maps.services.Status.OK) {
+                const coords = new window.kakao.maps.LatLng(
+                  result[0].y,
+                  result[0].x
+                );
 
-                            // 결과값으로 받은 위치를 마커로 표시합니다
-                            const marker = new window.kakao.maps.Marker({
-                                map: map,
-                                position: coords,
-                            });
+                console.log("coords", coords);
 
-                            // 인포윈도우로 장소에 대한 설명을 표시합니다
-                            const infowindow = new window.kakao.maps.InfoWindow({
-                                content: `<div style="padding:5px;">${userAddress}</div>`,
-                            });
-                            infowindow.open(map, marker);
-
-                            // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-                            map.setCenter(coords);
-                        }
-                    });
+                // 결과값으로 받은 위치를 마커로 표시합니다
+                const marker = new window.kakao.maps.Marker({
+                  map: map,
+                  position: coords,
                 });
-            };
-        } else {
-            console.error('로컬 스토리지에 데이터가 없습니다.');
-        }
-    }, []); // 컴포넌트가 마운트 될 때만 실행
 
-    return (
-        <>
-            <div className="container">
-                <A.contentWrap>
-                    <SearchComponent />
-                    <A.MapBox id="map"></A.MapBox> {/* 지도 영역 크기 설정 */}
-                </A.contentWrap>
-            </div>
-        </>
-    );
+                // 인포윈도우로 장소에 대한 설명을 표시합니다
+                const infowindow = new window.kakao.maps.InfoWindow({
+                  content: `<div style="padding:5px;">${userAddress}</div>`,
+                });
+                infowindow.open(map, marker);
+
+                // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+                map.setCenter(coords);
+              }
+            }
+          );
+        });
+      };
+    } else {
+      console.error("로컬 스토리지에 데이터가 없습니다.");
+    }
+  }, []); // 컴포넌트가 마운트 될 때만 실행
+
+  return (
+    <>
+      <div className="container">
+        <A.contentWrap>
+          <SearchComponent />
+          <A.MapBox id="map"></A.MapBox> {/* 지도 영역 크기 설정 */}
+        </A.contentWrap>
+      </div>
+    </>
+  );
 }
